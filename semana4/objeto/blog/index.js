47e515a24542c0fb96ad9759d05ebad29999a6fb
joaorpereira <jprp17@live.com>
2020-08-07 00:34:47 -0300

    soma modifications

--- semana4/objeto/blog/index.js
+++ semana4/objeto/blog/index.js
@@ -15,35 +15,43 @@ function getMessage(){
     return message.value
 }
 
+function getImage(){
+    const img = document.getElementById("img-post")
+    return img.value
+}
+
 function clickButton(event){
     const post = {
         title: getTitle(),
         autor: getAutor(),
         message: getMessage(),
+        img: getImage(),
     }
     posts.push(post)
     console.log(posts)
 
     const container = document.getElementById("container-de-posts")
-    container.innerHTML += `<h1>${post.title}</h1>` + `<h3>${post.autor}</h3>` + `<p>${post.message}</p>`
+    container.innerHTML += `<div class="post">`+`<h1>${post.title}</h1>` + 
+    `<h3>${post.autor}</h3>` + `<p>${post.img}</p>` + `<p>${post.message}</p>`+`</div>`
 
+    clearAll()
+}
+
+function pressEnter(event){
+    clickButton(event)
+}
+
+function clearAll(){
+    const clear_title = document.getElementById("titulo-post")
+    const clear_autor = document.getElementById("autor-post")
+    const clear_message = document.getElementById("conteudo-post")
+
+    clear_title.value = ""
+    clear_autor.value = ""
+    clear_message.value = ""    
 }
 
 
-/*function pressEnter(event){
-    if (event.key == 'Enter'){
-        getTitle()
-        getAutor()
-        getMessage()
-    }
+function pressEnter(event){
+    clickButton(event)
 }
-
-const post = {
-    title: titles,
-    autor: autors,
-    message: messages,
-}
-
-console.log(post)*/
-
-
